34e982bdb4b5e9ed30ccca402a0a25c0b458e150
BryanCruz <bryan.bialokur@gmail.com>
2018-09-25 19:40:27 -0300

    add more exercises

--- nine-nine-problems/src/Main.hs
+++ nine-nine-problems/src/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+result = res ++ "read*"
+  where res = "re" ++ res
+
 --7
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
@@ -27,6 +30,46 @@ pack (x:xs) = if x == w then [(x : z)] ++ (tail y) else [x] : y
 --10
 encode :: Eq a => [a] -> [(Int, a)]
 encode xs = [(length x, head x) | x <- (pack xs)]
+
+--11
+--data EncodedList = Single Int | Multiple Int Int
+--encodeModified ::  [EncodedList]
+--encodeModified = [(Single 1)]
+
+--14
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x : x : (dupli xs)
+
+--15
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) a = ys ++ (repli xs a)
+  where
+    ys = repliX a
+    repliX 0 = []
+    repliX b = x : repliX (b-1) 
+
+--16
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = dropEveryAux xs 1
+  where
+    dropEveryAux [] _
+      | otherwise = []
+    dropEveryAux (y:zs) a
+      | a == n    = dropEveryAux zs 1
+      | otherwise = y : dropEveryAux (zs) (a+1)
+
+--17
+split :: [a] -> Int -> ([a], [a])
+split [] _ = ([], [])
+split (x:xs) n
+  | n == 0    = ([], (x:xs))
+  | otherwise = (x:ys, zs)
+  where
+    vs = split xs (n-1)
+    ys = fst vs
+    zs = snd vs
 
 main :: IO ()
 main = do
