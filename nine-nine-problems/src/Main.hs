e2075821ae039425ad07748f7767703d4236255c
BryanCruz <bryan.bialokur@gmail.com>
2018-09-26 19:36:48 -0300

    feat: resolve exercises

--- nine-nine-problems/src/Main.hs
+++ nine-nine-problems/src/Main.hs
@@ -32,9 +32,21 @@ encode :: Eq a => [a] -> [(Int, a)]
 encode xs = [(length x, head x) | x <- (pack xs)]
 
 --11
---data EncodedList = Single Int | Multiple Int Int
---encodeModified ::  [EncodedList]
---encodeModified = [(Single 1)]
+data EncodedList a = Single a | Multiple Int a deriving (Show)
+encodeModified :: Eq a => [a] -> [EncodedList a]
+encodeModified []     = []
+encodeModified (x:xs) = result : (encodeModified (drop totalEq xs))
+  where
+    ys      = takeWhile (==x) xs
+    totalEq = length ys
+    result  = if (totalEq > 0) then (Multiple (totalEq+1) x) else (Single x) 
+
+--12
+decodeModified :: [EncodedList a] -> [a]
+decodeModified [] = []
+decodeModified ((Single x):xs)     = x : decodeModified xs
+decodeModified ((Multiple a x):xs) = [x | _ <- [1..a]] ++ decodeModified xs
+
 
 --14
 dupli :: [a] -> [a]
@@ -70,6 +82,43 @@ split (x:xs) n
     vs = split xs (n-1)
     ys = fst vs
     zs = snd vs
+
+--18
+slice :: [a] -> Int -> Int -> [a]
+slice []     _ _ =     []
+slice _      _ 0 =     [] 
+slice (x:xs) n m
+  | n >  m    = []
+  | n <= 1    = if   (m == 1)
+                then (x : [])
+                else (x : slice xs    1  (m-1))
+  | otherwise =  slice xs (n-1) (m-1)
+ 
+--19
+rotate :: [a] -> Int -> [a]
+rotate xs n
+  | n > 0 = rotate ((tail xs) ++ [y]) (n-1)
+  | n < 0 = rotate (z : (init xs))    (n+1)
+  | otherwise = xs
+  where
+    y = head xs
+    z = last xs
+
+rotate' :: [a] -> Int -> [a]
+rotate' xs n
+  | n >= 0 = (drop n xs) ++ (take n xs)
+  | n <  0 = reverse (rotate' (reverse xs) (-n))
+
+--20
+removeAt :: Int -> [a] -> (a, [a])
+removeAt _ [] = error "empty list"
+removeAt n (x:xs)
+  | n <= 0    = error "invalid index"
+  | n == 1    = (x, xs)
+  | otherwise = (fst next, x:(snd next))
+  where
+    next = removeAt (n-1) xs
+
 
 main :: IO ()
 main = do
